a3fae0b53cb2a30f862bc7624441754d88a79fea
Trenton Potgieter <darkreapyre@gmail.com>
2017-10-28 06:59:11 -0700

    Checkpoint

--- codebook/tests/trainer.py
+++ codebook/tests/trainer.py
@@ -28,7 +28,7 @@ redis_client = client('elasticache', region_name='us-west-2')
 cc = redis_client.describe_cache_clusters(ShowCacheNodeInfo=True)
 endpoint = cc['CacheClusters'][0]['CacheNodes'][0]['Endpoint']['Address']
 cache = redis(host=endpoint, port=6379, db=0)
-#global parameter_key
+global parameter_key
 #global parameters 
 #global results_key
 #global results
@@ -370,7 +370,7 @@ def lambda_handler(event, context):
     # Get the current state from the invoking lambda
     state = event.get('state')
     global parameters
-    global parameter_key
+#    global parameter_key
     parameter_key = event.get('parameter_key')
     parameters = from_cache(endpoint=endpoint, key=parameter_key)
     
