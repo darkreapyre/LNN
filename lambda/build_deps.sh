0a2ef50a3860b6663c5099e08154e714176b1422
Trenton Potgieter <darkreapyre@gmail.com>
2017-11-17 12:58:34 -0800

    REbuild dependencies as h5py has tests firectory

--- lambda/build_deps.sh
+++ lambda/build_deps.sh
@@ -3,12 +3,14 @@ virtualenv -p /usr/local/bin/python --always-copy --no-site-packages deps
 source deps/bin/activate
 
 pip install --upgrade pip wheel
-pip install -r requirements.txt
+pip install numpy scipy Pillow redis cfn-response
 
 libdir="$VIRTUAL_ENV/lib/python3.6/site-packages/"
 mkdir -p $libdir/lib || true
 echo "venv original size $(du -sh $VIRTUAL_ENV | cut -f1)"
 find $libdir -name "tests" | xargs rm -r
+# Manually install h5py as it has tests
+pip install h5py
 find $libdir -name "dataset" | xargs rm -rf
 find $libdir -name "*.pyc" -delete
 find $libdir -type d -empty -delete
