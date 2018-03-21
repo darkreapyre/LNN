dc7b7599077a765f04476e9da310416b6baeb179
Trenton <darkreapyre@gmail.com>
2018-03-21 17:26:52 +0000

    changed messaging

--- lambda/src/launch.py
+++ lambda/src/launch.py
@@ -408,7 +408,7 @@ def lambda_handler(event, context):
                     raise
 
                 # Publish to SNS
-                sns_message = "Training Completed EARLY!\n"+"Training halted at epoch {}".format(epoch)
+                sns_message = "Training Completed EARLY!\n"+"Training optimized at epoch {}".format(epoch)
                 sns_message += "\nFinal Cost = "+dumps(Costs[best_batch])
                 publish_sns(sns_message)
                 sys.exit()
