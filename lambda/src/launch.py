afe9f283417486a9c0f6e65134e547c47ea4bc0b
Trenton Potgieter <darkreapyre@gmail.com>
2018-03-21 10:12:27 -0700

    Process doesn't actually halt early, adding exit

--- lambda/src/launch.py
+++ lambda/src/launch.py
@@ -411,6 +411,7 @@ def lambda_handler(event, context):
                 sns_message = "Training Completed EARLY!\n"+"Training halted at epoch {}".format(epoch)
                 sns_message += "\nFinal Cost = "+dumps(Costs[best_batch])
                 publish_sns(sns_message)
+                sys.exit()
             
             # Update the results for this epoch with the average cost and
             # send status updates for epochs every 100 epochs
