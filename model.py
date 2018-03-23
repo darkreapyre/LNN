3246502a31a437a4b5ca8e2364cdd7969f5efba8
EC2 Default User <ec2-user@ip-172-16-4-121.us-west-2.compute.internal>
2018-03-23 01:33:13 +0000

    Checkpoint; switched to python 2.7

--- model.py
+++ model.py
@@ -1,19 +1,25 @@
 # Import necessary libraries
+from __future__ import print_function
 import os
 import logging
-import h5py
 import mxnet as mx
 import numpy as np
 from mxnet import nd, autograd, gluon
+"""
+Note: There is no sklearn and h5py support
+
+import h5py
 from sklearn.metrics import accuracy_score
+"""
 
 # ---------------------------------------------------------------------------- #
 #                            Training functions                                #
 # ---------------------------------------------------------------------------- #
 
-def train(channel_input_dirs, hyperparameters, hosts):
+def train(channel_input_dirs, hyperparameters, hosts, num_gpus, **kwargs):
     epochs = hyperparameters.get('epochs', 2500)
-    learning_rate = hyperparameters.get('learning_rate', 75e-4)
+    optmizer = hyperparameters.get('optmizer', 'sgd')
+    lr = hyperparameters.get('learning_rate', 75e-4)
     batch_size = hyperparameters.get('batch_size', 64)
     # Set logging
     logging.getLogger().setLevel(logging.DEBUG)
@@ -24,8 +30,67 @@ def train(channel_input_dirs, hyperparameters, hosts):
         kvstore = 'dist_device_sync' if num_gpus > 0 else 'dist_sync'
     # Set Context based on provided parameters
     ctx = [mx.gpu(i) for i in range(num_gpus)] if num_gpus > 0 else [mx.cpu()]
-    # Load Training Data
-    f_path = channel_input_dirs
+    print(channel_input_dirs)
+    print(os.listdir(channel_input_dirs['training']))
+    # Load Training/Testing Data
+    f_path = channel_input_dirs['training']
+    train_X, train_Y, test_X, test_Y = get_data(f_path)
+    num_examples = train_X.shape[0]
+    # Create Training and Test Data Iterators
+    train_data = mx.gluon.data.DataLoader(
+        mx.gluon.data.ArrayDataset(
+            train_X,
+            train_Y
+        ),
+        shuffle=True,
+        batch_size=batch_size
+    )
+    test_data = mx.gluon.data.DataLoader(
+        mx.gluon.data.ArrayDataset(
+            test_X,
+            test_Y
+        ),
+        shuffle=False,
+        batch_size=batch_size
+    )
+    # Initialize the network
+    net = build_network()
+    # Parameter Initialization
+    net.collect_params().initialize(mx.init.Xavier(magnitude=2.24))
+    # Optimizer
+    trainer = gluon.Trainer(net.collect_params(), optmizer, {'learning_rate': lr})
+    # Cross Entropy Loss Function
+    binary_ce = gluon.loss.SigmoidBinaryCrossEntropyLoss(from_sigmoid=True)
+    
+    # Start the Training loop
+    costs = [] # Track Loss function
+    for epoch in range(epochs):
+        cumulative_loss = 0
+        # Enumerate batches
+        for i, (data, label) in enumerate(train_data):
+            data = data.as_in_context(model_ctx)
+            label = label.as_in_context(model_ctx)
+            # Record for calculating derivatives for forward pass
+            with autograd.record():
+                output = net(data)
+                loss = binary_ce(output, label)
+            # Run backward pass
+            loss.backward()
+            trainer.step(data.shape[0])
+            cumulative_loss += nd.sum(loss).asscalar()
+        #test_accuracy = accuracy(test_data, net, test_Y)
+        costs.append(cumulative_loss/num_examples)
+        if epoch % 100 == 0:
+            #print("Epoch: {}; Loss: {}; Test Set Accuracy: {}"\
+            #      .format(epoch,cumulative_loss/num_examples,test_accuracy))
+            print("Epoch: {}; Loss: {}".format(epoch,cumulative_loss/num_examples))
+        elif epoch == epochs-1:
+            #print("Final Epoch: {}; Final Loss: {}; Final Test Set Accuracy: {}"\
+            #      .format(epoch,cumulative_loss/num_examples,test_accuracy))
+            print("Epoch: {}; Loss: {}".format(epoch,cumulative_loss/num_examples))
+    # Return the model for saving
+    return net
+                
 
 def build_network():
     """
@@ -55,7 +120,7 @@ def transform(x, y):
     x = x.reshape((x.shape[0], (x.shape[1] * x.shape[2]) * x.shape[3]))
     return x.astype(np.float32) / 255, y.astype(np.float32)
 
-def save(model, model_dir):
+def save(net, model_dir):
     """
     Saves the trained model to S3.
     
@@ -63,16 +128,14 @@ def save(model, model_dir):
     model -- The model returned from the `train()` function.
     model_dir -- The model directory location to save the model.
     """
-    # Create aplaceholder
-    n = net(mx.sym.var('data'))
-    # Save the model
-    n.save('%s/model.json' % model_dir)
-    # Save the Optimized Parameters
+    print("Saving the model in {}".format(model_dir))
+    y = net(mx.sym.var('data'))
+    y.save('%/model.json' % model_dir)
     net.collect_params().save('%s/model.params' % model_dir)
 
 def accuracy(data_iterator, net, Y):
     """
-    Evaluates the prediction against a test label.
+    Evaluates overall accuracy the prediction against a test label.
     
     Arguments:
     data_iterator -- Training data.
@@ -91,30 +154,24 @@ def accuracy(data_iterator, net, Y):
         Y = list(Y.flat)
     return accuracy_score(Y, y_pred)
 
-"""
-def get_data(f_path, channel):
+
+def get_data(f_path):
     """
-    Retrieves and loads the training data from S3.
+    Retrieves and loads the training/testing data from S3.
     
     Arguments:
-    f_path -- Location for the training input dataset.
+    f_path -- Location for the training/testing input dataset.
     
     Returns:
     Pre-processed training and testing data along with training and testing labels.
     """
-    # Import the Datasets
-    dataset = h5py.File(f_path, 'r')
-    train_set_x_orig = np.array(dataset['train_set_x'][:])
-    train_set_y_orig = np.array(dataset['train_set_y'][:])
-    test_set_x_orig = np.array(dataset['test_set_x'][:])
-    test_set_y_orig = np.array(dataset['test_set_y'][:])
-    # Pre-Process the data
-    train_X, train_Y = transform(train_set_x_orig, train_set_y_orig)
-    test_X, test_Y = transform(test_set_x_orig, test_set_y_orig)
+    train_X = np.load(os.path.join(f_path,'train/train_X.npy'))
+    train_Y = np.load(os.path.join(f_path,'train/train_Y.npy'))
+    train_X, train_Y = transform(train_X, train_Y)
+    test_X = np.load(os.path.join(f_path,'test/test_X.npy'))
+    test_Y = np.load(os.path.join(f_path,'test/vtest_Y.npy'))
+    test_X, test_Y = transform(test_X, test_Y)
     return train_X, train_Y, test_X, test_Y
-"""
-    
-    
     
     
     
