cd2f7affea7cc2875da8e996825c2842f204140d
Trenton Potgieter <darkreapyre@gmail.com>
2018-03-31 12:59:13 -0700

    model for Sagemake training

--- model.py
+++ model.py
@@ -1,41 +1,39 @@
 # Import necessary libraries
 from __future__ import print_function
+import boto3
 import os
+import io
 import logging
+import datetime
+import json
 import mxnet as mx
 import numpy as np
+from json import dumps, loads
 from mxnet import nd, autograd, gluon
-"""
-Note: There is no sklearn and h5py support
-
-import h5py
-from sklearn.metrics import accuracy_score
-"""
 
 # ---------------------------------------------------------------------------- #
 #                            Training functions                                #
 # ---------------------------------------------------------------------------- #
 
-def train(channel_input_dirs, hyperparameters, hosts, num_gpus, **kwargs):
+def train(channel_input_dirs, hyperparameters, hosts, num_gpus, output_data_dir, **kwargs):
     epochs = hyperparameters.get('epochs', 2500)
     optmizer = hyperparameters.get('optmizer', 'sgd')
     lr = hyperparameters.get('learning_rate', 75e-4)
     batch_size = hyperparameters.get('batch_size', 64)
     # Set logging
-    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.INFO)
     # Set Local vs. Distributed training
     if len(hosts) == 1:
         kvstore = 'device' if num_gpus > 0 else 'local'
     else:
         kvstore = 'dist_device_sync' if num_gpus > 0 else 'dist_sync'
     # Set Context based on provided parameters
-    ctx = [mx.gpu(i) for i in range(num_gpus)] if num_gpus > 0 else [mx.cpu()]
-    print(channel_input_dirs)
-    print(os.listdir(channel_input_dirs['training']))
+    ctx = mx.gpu() if num_gpus > 0 else mx.cpu()
     # Load Training/Testing Data
     f_path = channel_input_dirs['training']
     train_X, train_Y, test_X, test_Y = get_data(f_path)
     num_examples = train_X.shape[0]
+    
     # Create Training and Test Data Iterators
     train_data = mx.gluon.data.DataLoader(
         mx.gluon.data.ArrayDataset(
@@ -53,46 +51,52 @@ def train(channel_input_dirs, hyperparameters, hosts, num_gpus, **kwargs):
         shuffle=False,
         batch_size=batch_size
     )
+    
     # Initialize the network
-    net = build_network()
-    # Parameter Initialization
-    net.collect_params().initialize(mx.init.Xavier(magnitude=2.24))
+    net = create_graph()
+    # Parameter Initialization (Xavier)
+    net.collect_params().initialize(mx.init.Xavier(magnitude=2.34))
     # Optimizer
     trainer = gluon.Trainer(net.collect_params(), optmizer, {'learning_rate': lr})
     # Cross Entropy Loss Function
-    binary_ce = gluon.loss.SigmoidBinaryCrossEntropyLoss(from_sigmoid=True)
+    softmax_ce = gluon.loss.SoftmaxCrossEntropyLoss()
     
     # Start the Training loop
-    costs = [] # Track Loss function
+    results = {} # Track Loss function
+    results['Start'] = str(datetime.datetime.now())
     for epoch in range(epochs):
         cumulative_loss = 0
         # Enumerate batches
         for i, (data, label) in enumerate(train_data):
-            data = data.as_in_context(model_ctx)
-            label = label.as_in_context(model_ctx)
+            data = data.as_in_context(ctx)
+            label = label.as_in_context(ctx)
             # Record for calculating derivatives for forward pass
             with autograd.record():
                 output = net(data)
                 loss = binary_ce(output, label)
-            # Run backward pass
-            loss.backward()
+                # Run backward pass
+                loss.backward()
             trainer.step(data.shape[0])
             cumulative_loss += nd.sum(loss).asscalar()
-        #test_accuracy = accuracy(test_data, net, test_Y)
-        costs.append(cumulative_loss/num_examples)
+        # Accuracy Score
+        valid_accuracy = eval_acc(test_data, net)
+        train_accuracy = eval_acc(train_data, net)
+        results['epoch'+str(epoch)] = cumulative_loss/num_examples
         if epoch % 100 == 0:
-            #print("Epoch: {}; Loss: {}; Test Set Accuracy: {}"\
-            #      .format(epoch,cumulative_loss/num_examples,test_accuracy))
-            print("Epoch: {}; Loss: {}".format(epoch,cumulative_loss/num_examples))
+            print("Epoch: {}; Loss: {}; Train-accuracy = {}; Validation-accuracy = {}"\
+            .format(epoch,cumulative_loss/num_examples,train_accuracy,valid_accuracy))
         elif epoch == epochs-1:
-            #print("Final Epoch: {}; Final Loss: {}; Final Test Set Accuracy: {}"\
-            #      .format(epoch,cumulative_loss/num_examples,test_accuracy))
-            print("Epoch: {}; Loss: {}".format(epoch,cumulative_loss/num_examples))
+            print("Epoch: {}; Loss: {}; Train-accuracy = {}; Validation-accuracy = {}"\
+            .format(epoch,cumulative_loss/num_examples,train_accuracy,valid_accuracy))
+            results['end'] = str(datetime.datetime.now())
+    # Save the results
+    print("Saving the Training Results ...")
+    with open(str(output_data_dir)+'/results.json', 'w') as f:
+        json.dump(results, f)
     # Return the model for saving
     return net
                 
-
-def build_network():
+def create_graph():
     """
     Defines and Returns the Gluon Network Structure.
     """
@@ -101,7 +105,7 @@ def build_network():
         net.add(gluon.nn.Dense(20, activation='relu'))
         net.add(gluon.nn.Dense(7, activation='relu'))
         net.add(gluon.nn.Dense(5, activation='relu'))
-        net.add(gluon.nn.Dense(1, activation='sigmoid'))
+        net.add(gluon.nn.Dense(2, activation='relu'))
     net.hybridize()
     return net
 
@@ -128,32 +132,10 @@ def save(net, model_dir):
     model -- The model returned from the `train()` function.
     model_dir -- The model directory location to save the model.
     """
-    print("Saving the model in {}".format(model_dir))
+    print("Saving the trained model ...")
     y = net(mx.sym.var('data'))
-    y.save('%/model.json' % model_dir)
+    y.save('%s/model.json' % model_dir)
     net.collect_params().save('%s/model.params' % model_dir)
-
-def accuracy(data_iterator, net, Y):
-    """
-    Evaluates overall accuracy the prediction against a test label.
-    
-    Arguments:
-    data_iterator -- Training data.
-    net -- Gluon network.
-    Y -- True label.
-    
-    Returns:
-    Accuracy Score.
-    """
-    for i, (data, label) in enumerate(data_iterator):
-        data = data.as_in_context(model_ctx)
-        label = label.as_in_context(model_ctx)
-        output = net(data)
-        decision_boundary = np.vectorize(lambda x: 1 if x > 0.5 else 0)
-        y_pred = list(decision_boundary(output.asnumpy()).flat)
-        Y = list(Y.flat)
-    return accuracy_score(Y, y_pred)
-
 
 def get_data(f_path):
     """
@@ -165,14 +147,76 @@ def get_data(f_path):
     Returns:
     Pre-processed training and testing data along with training and testing labels.
     """
-    train_X = np.load(os.path.join(f_path,'train/train_X.npy'))
-    train_Y = np.load(os.path.join(f_path,'train/train_Y.npy'))
-    train_X, train_Y = transform(train_X, train_Y)
-    test_X = np.load(os.path.join(f_path,'test/test_X.npy'))
-    test_Y = np.load(os.path.join(f_path,'test/vtest_Y.npy'))
-    test_X, test_Y = transform(test_X, test_Y)
+    train_x = np.load(f_path+'/train_X.npy')
+    train_y = np.load(f_path+'/train_Y.npy')
+    train_X, train_Y = transform(train_x, train_y)
+    test_x = np.load(f_path+'/test_X.npy')
+    test_y = np.load(f_path+'/test_Y.npy')
+    test_X, test_Y = transform(test_x, test_y)
     return train_X, train_Y, test_X, test_Y
+
+# Evlauation metric
+"""
+Evaluates the Accuracy Score based on the input data and the results of the network.
+
+Arguments:
+data_iterator -- input data and associated label
+net -- Gluon model
+
+Returns:
+Accuracy score
+"""
+def eval_acc(data_iterator, net):
+    acc = mx.metric.Accuracy()
+    for i, (data, label) in enumerate(data_iterator):
+        data = data.as_in_context(model_ctx)
+        label = label.as_in_context(model_ctx)
+        output = net(data)
+        predictions = nd.argmax(output, axis=1)
+        acc.update(preds=predictions, labels=label)
+    return acc.get()[1]
+
+# ---------------------------------------------------------------------------- #
+#                           Hosting functions                                  #
+# ---------------------------------------------------------------------------- #
+
+def model_fn(model_dir):
+    """
+    Load the Gluon model for hosting.
+
+    Arguments:
+    model_dir -- SAgeMaker model directory.
+
+    Retuns:
+    Gluon model
+    """
+    # Load the saved Gluon model
+    symbol = mx.sym.load('%s/model.json' % model_dir)
+    outputs = mx.sym.sigmoid(data=symbol, name='sigmoid_label')
+    inputs = mx.sym.var('data')
+    param_dict = gluon.ParameterDict('model_')
+    net = gluon.SymbolBlock(outputs, inputs, param_dict)
+    net.load_params('%s/model.params' % model_dir, ctx=mx.cou())
+    return net
+
+def transform_fn(net, data, input_content_type, output_content_type):
+    """
+    Transform input data into prediction result.
+
+    Argument:
+    net -- Gluon model loaded from `model_fn()` function.
+    data -- Input data from the `InvokeEndpoint` request.
+    input_content_type -- Content type of the request (JSON).
+    output_content_type -- Disired content type (JSON) of the repsonse.
     
-    
-    
-    
+    Returns:
+    JSON paylod of the prediction result and content type.
+    """
+    # Parse the data
+    parsed = loads(data)
+    # Convert input to MXNet NDArray
+    nda = mx.nd.array(parsed)
+    output = net(nda)
+    prediction = (nd.sign(output) + 1) / 2
+    response_body = dumps(prediction.asnumpy().tolist()[0])
+    return response_body, output_content_type
