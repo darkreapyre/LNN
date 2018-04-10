80856d374c34d1fa815ad451d84aa689a19b5cb7
Trenton Potgieter <darkreapyre@gmail.com>
2018-04-09 21:30:46 -0700

    Tested code on sagemaker notebook; working; trying things around

--- src/app.py
+++ src/app.py
@@ -20,6 +20,16 @@ from skimage import transform
 
 build_id = str(os.environ['BUILD_ID'])[:7]
 print("Build ID: {}".format(build_id))
+print("Finding Endpoint config ...")
+sagemaker_client = boto3.client('sagemaker')
+list_results = sagemaker_client.list_endpoints(
+    SortBy='Name',
+    NameContains=build_id,
+    MaxResults=1,
+    StatusEquals='InService'
+)
+endpoint_name = str(list_results.get('Endpoints')[0]['EndpointName'])
+print("Endpoint Name: {}".format(endpoint_name))
 
 def process_url(url):
     """
@@ -50,17 +60,6 @@ def index():
 
 @app.route('/image')
 def image():
-
-    sagemaker_client = boto3.client('sagemaker')
-    list_results = sagemaker_client.list_endpoints(
-        SortBy='Name',
-        NameContains=build_id,
-        MaxResults=1,
-        StatusEquals='InService'
-    )
-    endpoint_name = str(list_results.get('Endpoints')[0]['EndpointName'])
-    print("Endpoint Name: {}".format(endpoint_name))
-
     print('api')
     url = request.args.get('image')
     print(url)
@@ -69,16 +68,20 @@ def image():
     image, payload = process_url(url)
 
     # Invoke the SageMaker endpoint
-    runtime = boto3.client(service_name='runtime.sagemaker')
-    response = runtime.invoke_endpoint(
-        EndpointName=endpoint_name,
-        ContentType='application/json',
-        Body=json.dumps(payload)
-    )
+    print("Invoking Endpoint ...")
+    #runtime = boto3.client(service_name='runtime.sagemaker')
+    runtime_client = boto3.client('sagemaker-runtime')
+    response = runtime_client.invoke_endpoint(
+    #response = runtime.invoke_endpoint(
+            EndpointName=endpoint_name,
+            ContentType='application/json',
+            Body=json.dumps(payload)
+        )
 
     # Format the prediction from SgeMaker Endpoint
     classes = ['non-cat', 'cat']
     prediction = classes[int(json.loads(response['Body'].read().decode('utf-8')))]
+    print("prediction: {}".format(prediction))
 
     # Return prediction and image
     figfile = BytesIO()
