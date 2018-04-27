1395376b5899bdfa0fd4543e7c0de32399391f8e
Trenton Potgieter <darkreapyre@gmail.com>
2018-04-27 09:40:06 -0700

    testing endpoint

--- src/endpoint.py
+++ src/endpoint.py
@@ -10,7 +10,7 @@ from sagemaker.mxnet import MXNet
 
 # Specify the Traiing Job to build against
 # Default: training_job = 0
-training_job = 0
+training_job = 'sagemaker-mxnet-2018-04-27-16-26-19-019'
 if training_job == 0:
     print("No Training job defined, exiting ...")
     sys.exit()
